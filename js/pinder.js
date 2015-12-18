3a83f239ea46c631eed6ebd88868d0bef00529f8
Ryan <bluesourcebeck@gmail.com>
2015-12-18 14:28:31 -0500

    Front

---
+++ js/pinder.js
@@ -0,0 +1,40 @@
+// Closes the sidebar menu
+$("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+// Grab a random background picture
+var bg = {
+    changeBg : function (){
+            var bgSrc='http://lorempixel.com/1024/768/';
+            var bgValue = 'url(' + bgSrc + ') no-repeat center top scroll';
+            $('.header').css('background', bgValue);
+            return bgValue;
+    },
+};
+
+bg.changeBg();
